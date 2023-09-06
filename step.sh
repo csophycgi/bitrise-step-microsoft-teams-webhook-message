ec7491f316fa26d84ce148fa3745092990991a4d
Chris <christophe.sophy.ext@cgi.com>
2023-09-06 11:57:34 +0200

    try repair v2

--- step.sh
+++ step.sh
@@ -4,7 +4,7 @@ set -ex
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 node -v
-npm init -force
+mkdir -p $THIS_SCRIPT_DIR/node_modules
 npm install --prefix $THIS_SCRIPT_DIR axios moment-timezone --save
 ls -la $THIS_SCRIPT_DIR
 
