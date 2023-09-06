4f1850d624619933fec9f6a7c582035510202839
Chris <christophe.sophy.ext@cgi.com>
2023-09-06 12:03:58 +0200

    try repair v4

--- step.sh
+++ step.sh
@@ -3,9 +3,6 @@ set -ex
 
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-node -v
-mkdir -p $THIS_SCRIPT_DIR/node_modules
-npm install --prefix $THIS_SCRIPT_DIR axios moment-timezone --save
-ls -la $THIS_SCRIPT_DIR/node_modules
+npm install --prefix $THIS_SCRIPT_DIR axios moment-timezone --save --verbose
 
 $THIS_SCRIPT_DIR/step.js "${is_debug_mode}" "${webhook_url}" "${team_id}" "${channel_id}" "${app_avatar_api_token}" "${preset_status}"
