49c81deb6658d0a60c128e0dce2dad6cb7759ffd
Chris <christophe.sophy.ext@cgi.com>
2023-09-06 12:08:53 +0200

    try repair v5

--- step.sh
+++ step.sh
@@ -3,6 +3,7 @@ set -ex
 
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+export NODE_ENV=
 npm install --prefix $THIS_SCRIPT_DIR axios moment-timezone --save --verbose
 
 $THIS_SCRIPT_DIR/step.js "${is_debug_mode}" "${webhook_url}" "${team_id}" "${channel_id}" "${app_avatar_api_token}" "${preset_status}"
