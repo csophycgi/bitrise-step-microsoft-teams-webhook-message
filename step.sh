8bf97a05cc6c86d748a15a71e591fa6feca786c5
Chris <christophe.sophy.ext@cgi.com>
2023-09-06 11:50:19 +0200

    try repair bitrise v1

--- step.sh
+++ step.sh
@@ -6,6 +6,6 @@ THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 node -v
 npm init -force
 npm install --prefix $THIS_SCRIPT_DIR axios moment-timezone --save
-ls -la $THIS_SCRIPT_DIR/node_modules
+ls -la $THIS_SCRIPT_DIR
 
 $THIS_SCRIPT_DIR/step.js "${is_debug_mode}" "${webhook_url}" "${team_id}" "${channel_id}" "${app_avatar_api_token}" "${preset_status}"
